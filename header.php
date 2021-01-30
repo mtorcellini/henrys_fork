63e548aa45c08c4d6126b1cfa4662bde9648edf1
mtorcellini <matt.torcellini@gmail.com>
2021-01-30 12:57:40 -0500

    add lines to burger menu

--- header.php
+++ header.php
@@ -46,7 +46,12 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'henrys_fork' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+        <!-- <?php esc_html_e( 'Primary Menu', 'henrys_fork' ); ?> -->
+        <div class="burgerDiv"></div>
+        <div class="burgerDiv"></div>
+        <div class="burgerDiv"></div>
+      </button>
 			<?php
 			wp_nav_menu(
 				array(
