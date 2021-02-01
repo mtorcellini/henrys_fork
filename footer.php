84108fd9ae903d50707aa8be13429064db54ab7c
mtorcellini <matt.torcellini@gmail.com>
2021-02-01 18:17:21 -0500

    footer message

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'henrys_fork' ), 'henrys_fork', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'henrys_fork' ), 'henrys_fork', '<a href="http://tortdesigns.com/">Matt Torcellini</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
