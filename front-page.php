32a95faf7753c32b785f3c8324a7d0df6b9a4893
mtorcellini <matt.torcellini@gmail.com>
2021-02-03 13:49:47 -0500

    add content to home

--- front-page.php
+++ front-page.php
@@ -65,8 +65,27 @@
 
 		<?php
 		while ( have_posts() ) :
-			the_post();
-			get_template_part( 'template-parts/content', 'page' );
+      the_post();
+      // Remove standard template for pages
+      // get_template_part( 'template-parts/content', 'page' );
+      ?>
+
+      <div class="entry-content">
+        <?php
+        the_content();
+
+        wp_link_pages(
+          array(
+            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'henrys_fork' ),
+            'after'  => '</div>',
+          )
+        );
+        ?>
+      </div><!-- .entry-content -->
+
+
+
+      <?php
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
