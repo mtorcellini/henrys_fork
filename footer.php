d8f0cc4cc286363ef380584da9e5dd26ccbbbf97
mtorcellini <matt.torcellini@gmail.com>
2021-04-28 14:18:15 -0400

    style searchbar and remove footer copy

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'henrys_fork' ) ); ?>">
+			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'henrys_fork' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'henrys_fork' ), 'WordPress' );
@@ -23,7 +23,7 @@
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'henrys_fork' ), 'henrys_fork', '<a href="http://tortdesigns.com/">Matt Torcellini</a>' );
-				?>
+				?> -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
